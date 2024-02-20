0b5e54431dbd4e5b0ebe9dc9707c6f098c3265f1
srzorro <srzorro@gmail.com>
2024-02-19 20:23:59 -0500

    :sparkles: Creando el  modelo artista

--- Api-Rest-Node-Musica/controllers/artist.js
+++ Api-Rest-Node-Musica/controllers/artist.js
@@ -1,3 +1,6 @@
+// Importaciones
+const Artist = require("../models/artist");
+
 // accion de prueba
 const artist = (req, res) => {
   return res.status(200).send({
@@ -5,6 +8,8 @@ const artist = (req, res) => {
     message: "Mensaje enviado desde controllers/artist.js",
   });
 };
+
+// Accion guardar artista
 
 module.exports = {
   artist,
